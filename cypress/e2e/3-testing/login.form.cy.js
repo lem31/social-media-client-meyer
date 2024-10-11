00f7c5cb2d5c814fb88fb373a51cd21f24512d7e
Leanne <lemeyer.no@gmail.com>
2024-10-11 17:09:32 +0200

    had to update the test code in login.form.cy.js to ensure that cypress actually closes the form below the login form before trying to type in the login form, as it was not working properly, now works fine

--- cypress/e2e/3-testing/login.form.cy.js
+++ cypress/e2e/3-testing/login.form.cy.js
@@ -7,15 +7,16 @@ describe('Login Function Test', () => {
   it('should login successfully with correct credentials', () => {
     cy.visit('/index.html');
 
-    cy.get('#closeButton').click();
+    cy.get('#closeButton')
+      .should('be.visible')
+      .wait(500)
+      .click({ force: true });
 
     // Fill out the login form using IDs
-    cy.get('#loginEmail')
-      .first()
-      .type('fatherchristmas@stud.noroff.no');
-    cy.get('#loginPassword')
-      .first()
-      .type('fatherchristmas222');
+    cy.get('#loginEmail').type(
+      'fatherchristmas@stud.noroff.no',
+    );
+    cy.get('#loginPassword').type('fatherchristmas222');
 
     // Intercept the login request
     cy.intercept('POST', `${apiPath}/social/auth/login`).as(
